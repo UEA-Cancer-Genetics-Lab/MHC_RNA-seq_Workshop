09d8d629118590e1fdd8102185549ffc7ff37469
Sergio Llaneza <sergiollanezalago@gmail.com>
2025-07-15 08:50:08 +0100

    Adds a backup script in case first one fails

---
+++ download_referece_backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p reference_files && \
+    wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.pc_transcripts.fa.gz && \
+    gunzip gencode.v19.pc_transcripts.fa.gz && \
+    mv gencode.v19.pc_transcripts.fa reference_files/gencode.v19.pc_transcripts.fa
+
+wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz && \
+    gunzip gencode.v19.annotation.gtf.gz && \
+    mv gencode.v19.annotation.gtf reference_files/gencode.v19.annotation.gtf && \
+    echo "Files in /reference_files:" && \
+    ls -lh reference_files
+
+salmon index -t reference_files/gencode.v19.pc_transcripts.fa -i reference_files/salmon_index --gencode -k 7
+
+mkdir -p results/fastqc
+mkdir -p results/fastp
